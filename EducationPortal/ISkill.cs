fa283793f65b050edb338f4584221316546e7c87
Mortem <deusvecumest@gmail.com>
2021-04-29 23:12:12 +0300

    Adding a primitive class diagram

---
+++ EducationPortal/ISkill.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EducationPortal
+{
+    public interface ISkill
+    {
+    }
+
+    public interface CopyOfISkill
+    {
+    }
+}
---
+++ EducationPortal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EducationPortal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
